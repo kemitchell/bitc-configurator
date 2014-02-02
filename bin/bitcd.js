f96719b535233e3bda3693a8d6d62d01d79a6ae9
Josh The Geek <joshthegeek@me.com>
2014-02-01 22:14:14 -0500

    Reorganized public stuff

--- bin/bitcd.js
+++ bin/bitcd.js
@@ -3,6 +3,7 @@
 // Copyright(c) 2014 Joshua Oldenburg (http://joshuaoldenburg.com)
 // Released under the GPLv3 license. See LICENSE.txt for more info
 
+var util = require('util');
 var server = require('../lib/bitcd');
 var argv = require('optimist')
 	.describe('port', 'The port to listen on').alias('port', 'p').default('port', process.env.port || 9000)
@@ -10,6 +11,12 @@ var argv = require('optimist')
 	.describe('password', 'Basic auth password').alias('password', 'pass')
 	.argv;
 
-server.createServer(argv, function() {
-	console.log('[BITCd] Listening on port %d with options %j', argv.port, argv);
+var options = {
+	port: argv.port,
+	username: argv.username,
+	password: argv.password,
+};
+
+server.createServer(options, function() {
+	console.log('[BITCd] Listening on port %d with options %s', argv.port, util.inspect(options, { colors: true }));
 });
