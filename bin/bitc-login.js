aa69563173e4d15370a129458411d69d3e326687
Josh The Geek <joshthegeek@me.com>
2014-03-10 20:52:38 -0400

    Implement the requesting of the computer ID by "bitc login".

--- bin/bitc-login.js
+++ bin/bitc-login.js
@@ -5,8 +5,11 @@
 
 var util = require('util');
 var api = require('../lib/api');
+var request = require('request');
+
 var cmd = require('commander').version(api.version);
 var argv = cmd.parse(process.argv);
+var computerID = cmd.args[0];
 
 if (cmd.args.length != 1) {
 	console.error('Error: you must have one and only one argument: the computer to log in to');
@@ -17,3 +20,22 @@ if (!api.thisComputer.isAdmin) {
 	console.error('Error: only admins can log in to other computers');
 	process.exit(1);
 }
+
+request('http://10.0.0.1/api/1/computers/byID', {
+	json: true,
+}, function(error, response, body) {
+	if (error || response.statusCode == 200 || body.error) {
+		console.error('Error: failed to load computers');
+		process.exit(1);
+		return;
+	}
+
+	if (!body[computerID] || !body[computerID].computerIP) {
+		console.error('Error: no registered computer has that ID');
+		process.exit(1);
+		return;
+	}
+
+	console.log(body[computerID].computerIP);
+	process.exit(0);
+});
